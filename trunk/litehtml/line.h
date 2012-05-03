b9a7440dafee2413add04237fe55f55f5a54dd29
yuri.kobets@gmail.com <yuri.kobets@gmail.com@f8642104-a71d-779b-ab08-f639ff56674e>
2012-05-03 02:33:03 +0000

    Added text-align CSS property handling.

--- trunk/litehtml/line.h
+++ trunk/litehtml/line.h
@@ -22,9 +22,16 @@ namespace litehtml
 		int				m_padding_top;
 		element_clear	m_clear;
 		bool			m_last_white_space;
+		int				m_min_height;
+
+		int				m_line_left;
+		int				m_line_right;
 	public:
 		line(void)
 		{
+			m_line_left			= 0;
+			m_line_right		= 0;
+			m_min_height		= 0;
 			m_padding_bottom	= 0;
 			m_padding_top		= 0;
 			m_height			= 0;
@@ -39,6 +46,9 @@ namespace litehtml
 
 		line(const line& val)
 		{
+			m_line_left			= val.m_line_left;
+			m_line_right		= val.m_line_right;
+			m_min_height		= val.m_min_height;
 			m_items				= val.m_items;
 			m_height			= val.m_height;
 			m_top_margin		= val.m_top_margin;
@@ -70,6 +80,9 @@ namespace litehtml
 			m_padding_top		= 0;
 			m_clear				= clear_none;
 			m_last_white_space	= true;
+			m_min_height		= 0;
+			m_line_left			= 0;
+			m_line_right		= 0;
 		}
 
 		void	operator+=(element* el);
@@ -78,7 +91,7 @@ namespace litehtml
 
 		int		get_height() const			{	return m_height;			}
 		int		get_left() const			{	return m_left;				}
-		void	set_left(int left) 			{	m_left = left;				}
+		void	init(int left, int right, int top, int line_height);
 		int		get_top() const				{	return m_top;				}
 		int		get_margin_top() const		{	return m_top_margin;		}
 		int		get_margin_bottom() const	{	return m_bottom_margin;		}
@@ -86,9 +99,12 @@ namespace litehtml
 		bool	empty()	const				{	return m_items.empty();		}
 		bool	is_white_space() const		{	return m_last_white_space;	}
 		void	get_elements(elements_vector& els);
-		bool	finish();
+		bool	finish(text_align align);
 		bool	collapse_top_margin()		{	return (m_padding_top == 0);	}
 		bool	collapse_bottom_margin()	{	return (m_padding_bottom == 0);	}
+		bool	have_room_for(element* el);
+		int		line_right()				{	return m_line_right;			}
+		int		line_left()					{	return m_line_left;				}
 		
 		element_clear get_clear_floats() const { return m_clear; }
 	};
