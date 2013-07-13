0879ce0b6d50b6331d5a61362fcf51baf33003ab
yuri.kobets@gmail.com <yuri.kobets@gmail.com@f8642104-a71d-779b-ab08-f639ff56674e>
2013-07-13 21:13:39 +0000

    Fixed incorrect element height in some cases. <TD> tag inherits the background styles from parent <TR> tag.

--- trunk/litehtml/el_td.h
+++ trunk/litehtml/el_td.h
@@ -9,6 +9,7 @@ namespace litehtml
 		el_td(litehtml::document* doc);
 		virtual ~el_td();
 
-		virtual void	parse_styles(bool is_reparse);
+		virtual void			parse_styles(bool is_reparse);
+		virtual const wchar_t*	get_style_property(const wchar_t* name, bool inherited, const wchar_t* def = 0);
 	};
 }
