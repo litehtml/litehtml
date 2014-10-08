d6f5885ba8f6ad238368e112cd1da4f6138d0872
Yuri Kobets <yuri.kobets@gmail.com>
2014-10-09 02:31:37 +0400

    Added default predefined value parameter into css_length::fromString This fixes the bug in CSS text when length is not a number or valid keyword

--- src/css_length.cpp
+++ src/css_length.cpp
@@ -1,7 +1,7 @@
 #include "html.h"
 #include "css_length.h"
 
-void litehtml::css_length::fromString( const tstring& str, const tstring& predefs )
+void litehtml::css_length::fromString( const tstring& str, const tstring& predefs, int defValue )
 {
 	// TODO: Make support for calc
 	if(str.substr(0, 4) == _t("calc"))
@@ -40,7 +40,15 @@ void litehtml::css_length::fromString( const tstring& str, const tstring& predef
 				un += *chr;
 			}
 		}
-		m_value = (float) t_strtod(num.c_str(), 0);
-		m_units	= (css_units) value_index(un.c_str(), css_units_strings, css_units_none);
+		if(!num.empty())
+		{
+			m_value = (float) t_strtod(num.c_str(), 0);
+			m_units	= (css_units) value_index(un.c_str(), css_units_strings, css_units_none);
+		} else
+		{
+			// not a number so it is predefined
+			m_is_predefined = true;
+			m_predef = defValue;
+		}
 	}
 }
