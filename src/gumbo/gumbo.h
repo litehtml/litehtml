ae3a212cbfea041a3984f25156735c6b5531ead5
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2015-02-12 12:18:10 +0100

    dont' define _CRT_SECURE_NO_WARNINGS if already defined.

--- src/gumbo/gumbo.h
+++ src/gumbo/gumbo.h
@@ -43,8 +43,10 @@
 #define GUMBO_GUMBO_H_
 
 #ifdef _MSC_VER
-#define _CRT_SECURE_NO_WARNINGS
-#define fileno _fileno
+    #ifndef _CRT_SECURE_NO_WARNINGS
+        #define _CRT_SECURE_NO_WARNINGS
+    #endif
+    #define fileno _fileno
 #endif
 
 #ifndef _MSC_VER
