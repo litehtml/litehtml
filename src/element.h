3cee65c0442d0c60cb4f4ee19fd5ade5f7bb8463
Arkarit <phoenix@phoenixgrafik.de>
2015-12-30 18:05:38 +0100

    Defined const_ptr in element and let document get_over_element return this const pointer

--- src/element.h
+++ src/element.h
@@ -15,8 +15,9 @@ namespace litehtml
 		friend class el_table;
 		friend class document;
 	public:
-		typedef std::shared_ptr<litehtml::element>		ptr;
-		typedef std::weak_ptr<litehtml::element>		weak_ptr;
+		typedef std::shared_ptr<litehtml::element>			ptr;
+		typedef std::shared_ptr<const litehtml::element>	const_ptr;
+		typedef std::weak_ptr<litehtml::element>			weak_ptr;
 	protected:
 		std::weak_ptr<element>		m_parent;
 		std::weak_ptr<litehtml::document>	m_doc;
