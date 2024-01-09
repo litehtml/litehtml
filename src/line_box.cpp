6ad0c533c4663b0ebef0fd8789a88e1ac42552b3
Yuri Kobets <yuri.kobets@gmail.com>
2024-01-09 23:50:17 +0300

    fixed rendering of <br> tag with the 'clear' property

--- src/line_box.cpp
+++ src/line_box.cpp
@@ -555,6 +555,12 @@ bool litehtml::line_box::can_hold(const std::unique_ptr<line_box_item>& item, wh
 
 	if(item->get_type() == line_box_item::type_text_part)
 	{
+		// force new line on floats clearing
+		if (item->get_el()->src_el()->is_break() && item->get_el()->src_el()->css().get_clear() != clear_none)
+		{
+			return false;
+		}
+
 		auto last_el = get_last_text_part();
 
 		// the first word is always can be hold
@@ -564,7 +570,8 @@ bool litehtml::line_box::can_hold(const std::unique_ptr<line_box_item>& item, wh
 		}
 
 		// force new line if the last placed element was line break
-		if (last_el && last_el->src_el()->is_break())
+		// Skip If there are the only break item - this is float clearing
+		if (last_el && last_el->src_el()->is_break() && m_items.size() > 1)
 		{
 			return false;
 		}
@@ -603,6 +610,12 @@ bool litehtml::line_box::have_last_space()  const
 bool litehtml::line_box::is_empty() const
 {
     if(m_items.empty()) return true;
+	if(m_items.size() == 1 &&
+		m_items.front()->get_el()->src_el()->is_break() &&
+		m_items.front()->get_el()->src_el()->css().get_clear() != clear_none)
+	{
+		return true;
+	}
     for (const auto& el : m_items)
     {
 		if(el->get_type() == line_box_item::type_text_part)
