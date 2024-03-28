e980ab70139c6c1ca81f8c1ee1cc5a5874d9f60b
Yuri Kobets <yuri.kobets@gmail.com>
2024-03-29 02:52:25 +0300

    fix test_container

--- containers/test/test_container.h
+++ containers/test/test_container.h
@@ -23,6 +23,7 @@ public:
 	void			draw_solid_fill(litehtml::uint_ptr hdc, const background_layer& layer, const web_color& color) override;
 	void			draw_linear_gradient(litehtml::uint_ptr /*hdc*/, const background_layer& /*layer*/, const background_layer::linear_gradient& /*gradient*/) override {};
 	void			draw_radial_gradient(litehtml::uint_ptr /*hdc*/, const background_layer& /*layer*/, const background_layer::radial_gradient& /*gradient*/) override {};
+	void 			draw_conic_gradient(litehtml::uint_ptr /*hdc*/, const litehtml::background_layer& /*layer*/, const litehtml::background_layer::conic_gradient& /*gradient*/) override {};
 	void			draw_borders(uint_ptr hdc, const borders& borders, const position& draw_pos, bool root) override;
 	void 			draw_list_marker(uint_ptr hdc, const list_marker& marker) override;
 	element::ptr	create_element(const char* /*tag_name*/,
