3efe3cb87f7e74a22bb460e8a1db74c9aa1cbe32
Yuri Kobets <yuri.kobets@gmail.com>
2024-03-30 23:47:35 +0300

    utf8: use char32_t in utf8 conversions.
    utf8_to_wchar is renamed to utf8_to_utf32
    wchar_to_utf8 is renamed to utf32_to_utf8

--- include/litehtml/utf8_strings.h
+++ include/litehtml/utf8_strings.h
@@ -7,36 +7,36 @@
 namespace litehtml
 {
 	// converts UTF-32 ch to UTF-8 and appends it to str
-	void append_char(string& str, int ch);
+	void append_char(string& str, char32_t ch);
 
-	class utf8_to_wchar
+	class utf8_to_utf32
 	{
 		const byte* m_utf8;
-		std::wstring m_str;
+		std::u32string m_str;
 	public:
-		utf8_to_wchar(const char* val);
-		operator const wchar_t*() const
+		utf8_to_utf32(const char* val);
+		operator const char32_t*() const
 		{
 			return m_str.c_str();
 		}
 	private:
-		ucode_t getb()
+		char32_t getb()
 		{
 			if (!(*m_utf8)) return 0;
 			return *m_utf8++;
 		}
-		ucode_t get_next_utf8(ucode_t val)
+		char32_t get_next_utf8(char32_t val)
 		{
 			return (val & 0x3f);
 		}
-		ucode_t get_char();
+		char32_t get_char();
 	};
 
-	class wchar_to_utf8
+	class utf32_to_utf8
 	{
 		std::string m_str;
 	public:
-		wchar_to_utf8(const std::wstring& val);
+		utf32_to_utf8(const std::u32string& val);
 		operator const char*() const
 		{
 			return m_str.c_str();
@@ -48,8 +48,8 @@ namespace litehtml
 		}
 	};
 
-#define litehtml_from_wchar(str)	litehtml::wchar_to_utf8(str)
-#define litehtml_to_wchar(str)		litehtml::utf8_to_wchar(str)
+#define litehtml_from_utf32(str)	litehtml::utf32_to_utf8(str)
+#define litehtml_to_utf32(str)		litehtml::utf8_to_wchar(str)
 }
 
 #endif  // LH_UTF8_STRINGS_H
